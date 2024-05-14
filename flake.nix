245b5a287740a026ddb01b506a4b49d3a2893cec
sobte <i@sobte.me>
2024-05-15 01:14:07 +0800

    feat: add garnix cache

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   description = "Meow' Nix Flakes";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.garnix.io"
+    ];
+    extra-trusted-public-keys = [
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+  };
+
   inputs = {
     flake-schemas.url = "github:DeterminateSystems/flake-schemas";
 
