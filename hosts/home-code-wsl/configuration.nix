26bd89f8b9738397b952a3c6ec02a73cd95df201
sobte <i@sobte.me>
2024-06-16 22:51:24 +0800

    feat: use gnome to wsl

--- hosts/home-code-wsl/configuration.nix
+++ hosts/home-code-wsl/configuration.nix
@@ -5,7 +5,7 @@
     # <nixos-wsl/modules>
     ../../modules/linux/core/services/samba.nix
     ../../modules/linux/core/services/vscode-server.nix
-    ../../modules/linux/desktop/display/kde
+    ../../modules/linux/desktop/display/gnome
     ../../modules/linux/desktop/configuration.nix
   ];
 
--- modules/linux/desktop/display/gnome/default.nix
+++ modules/linux/desktop/display/gnome/default.nix
@@ -5,7 +5,6 @@
   ];
 
   services.xserver = {
-    displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
   };
 }
