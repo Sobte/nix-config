14062ee1a769fa5b3830bde4afb25df17f332e6e
sobte <i@sobte.me>
2024-06-22 03:02:19 +0800

    feat: add vscode-server to dev-server

--- hosts/home-dev-server/configuration.nix
+++ hosts/home-dev-server/configuration.nix
@@ -4,6 +4,8 @@
     ../../modules/linux/server.nix
     # home samba
     ../../modules/linux/packages/core/services/samba.nix
+    # vscode server
+    ../../modules/linux/packages/core/services/vscode-server.nix
   ];
 
   # packages installed in system profile. to search by name, run:
