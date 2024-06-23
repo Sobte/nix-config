735323c73c255752a8f1092ec4f175ccaf633dc6
sobte <i@sobte.me>
2024-06-23 18:30:35 +0800

    feat: add home-dev-server wg.conf

--- hosts/home-dev-server/configuration.nix
+++ hosts/home-dev-server/configuration.nix
@@ -1,3 +1,4 @@
+{ config, host, ... }:
 {
   imports = [
     ../../modules/linux/server.nix
@@ -10,4 +11,14 @@
   # packages installed in system profile. to search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = [ ];
+
+  # wg-quick configuration
+  networking.wg-quick.interfaces = {
+    wg-come-home = {
+      # TODO use hosts-secrets (garnix no cache secrets)
+      configFile = "${
+        config.users.users.${host.username}.home
+      }/.config/hosts-secrets/hosts/home-dev-server/wg-come-home.conf";
+    };
+  };
 }
