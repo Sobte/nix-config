1919214398afa83fa143278a9ac87a18d6ab99a0
sobte <i@sobte.me>
2024-06-28 17:02:03 +0800

    feat: remove the debugfs mount

--- modules/linux/packages/core/boot/loader/container.nix
+++ modules/linux/packages/core/boot/loader/container.nix
@@ -1,1 +1,10 @@
-{ boot.isContainer = true; }
+{
+  boot.isContainer = true;
+  # remove the debugfs mount
+  systemd.mounts = [
+    {
+      where = "/sys/kernel/debug";
+      enable = false;
+    }
+  ];
+}
