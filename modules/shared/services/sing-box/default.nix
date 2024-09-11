53b3dc5b467388aee93a08fffea34c3f67a6899c
sobte <i@sobte.me>
2024-09-11 12:54:18 +0800

    feat: add sing-box

---
+++ modules/shared/services/sing-box/default.nix
@@ -0,0 +1,40 @@
+{
+  pkgs,
+  config,
+  lib,
+  namespace,
+  ...
+}:
+let
+  inherit (pkgs.stdenv) isLinux;
+
+  cfg = config.${namespace}.shared.services.sing-box;
+in
+{
+  options.${namespace}.shared.services.sing-box = {
+    enable = lib.mkEnableOption "sing-box";
+    configName = lib.mkOption {
+      type = lib.types.str;
+      default = "config.json";
+    };
+  };
+
+  config = lib.mkIf cfg.enable (
+    {
+      # secrets
+      ${namespace}.shared.secrets.shared.sing-box.configFiles = [ { name = cfg.configName; } ];
+      # sing-box configuration
+      environment.etc = {
+        "sing-box/config.json" = {
+          source = config.age.secrets."sing-box-${cfg.configName}".path;
+        };
+      };
+
+    }
+    // (lib.optionalAttrs isLinux {
+      # enable sing-box
+      services.sing-box.enable = true;
+    })
+  );
+
+}
