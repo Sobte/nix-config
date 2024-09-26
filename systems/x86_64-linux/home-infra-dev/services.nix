e4312c1825398de74339150ead85416ad25c9d3f
sobte <i@sobte.me>
2024-09-26 18:36:19 +0800

    feat: add infra-dev gitea conf

--- systems/x86_64-linux/home-infra-dev/services.nix
+++ systems/x86_64-linux/home-infra-dev/services.nix
@@ -1,23 +1,13 @@
 { namespace, ... }:
-# let
-#   domain = "git.sobte.dev";
-# in
 {
   ${namespace} = {
     services = {
       # Do not add ensureDatabases yet because gitea will add it.
       postgresql.enable = true;
-      # acme = {
-      #   useRoot = true;
-      #   certs.${domain} = { };
-      # };
-      # nginx = {
-      #   enable = true;
-      #   secrets.configNames = [ "${domain}" ];
-      # };
       gitea = {
         enable = true;
         dbBackend = "postgresql";
+        useWizard = true;
       };
     };
     shared.services.wg-quick.configNames = [ "wg-come-home" ];
