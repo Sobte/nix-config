427fa734263a8f8575d0959067c20c24948a979e
sobte <i@sobte.me>
2024-09-26 23:22:49 +0800

    feat: add cloudflared to infra-dev

--- systems/x86_64-linux/home-infra-dev/services.nix
+++ systems/x86_64-linux/home-infra-dev/services.nix
@@ -9,6 +9,17 @@
         dbBackend = "postgresql";
         useWizard = true;
       };
+      cloudflared = {
+        enable = true;
+        tunnels = {
+          "466fcedf-48d6-4066-89e4-069b29d27cd8" = {
+            default = "http_status:404";
+            ingress = {
+              "git.sobte.dev" = "http://localhost:3000";
+            };
+          };
+        };
+      };
     };
     shared.services.wg-quick.configNames = [ "wg-come-home" ];
   };
