bcbb2bb4df364e823bb56b291ca97f822edec93a
sobte <i@sobte.me>
2024-09-29 18:12:08 +0800

    feat: move home-infra-relay host

--- systems/x86_64-linux/home-infra-relay/default.nix
+++ systems/x86_64-linux/home-infra-relay/default.nix
@@ -1,12 +1,14 @@
 { namespace, ... }:
 {
+  imports = [ ./hardware.nix ];
+
   ${namespace} = {
     user.name = "root"; # use root as default user
     room.server.enable = true;
-    # r5s requires a specific bootloader.
-    system.boot.efi.enable = false;
-    # autologin
     services.getty.enable = true;
+    shared.services.sing-box = {
+      enable = true;
+    };
   };
 
   system.stateVersion = "24.11";
