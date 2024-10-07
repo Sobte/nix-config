07faef6aa7b067e58766b31087ddbb1c7f84dedd
sobte <i@sobte.me>
2024-10-08 00:48:05 +0800

    feat: add minimal-no-zfs home

---
+++ homes/x86_64-install-iso/nixos@minimal-no-zfs/default.nix
@@ -0,0 +1,13 @@
+{
+  home.stateVersion = "24.11";
+
+  cattery = {
+    room.general.enable = true;
+    # convenient to check specifications during first installation
+    cli-apps.tool = {
+      fastfetch.enable = true;
+      speedtest.enable = true;
+    };
+  };
+
+}
--- modules/darwin/brew/default.nix
+++ modules/darwin/brew/default.nix
@@ -14,9 +14,6 @@ in
 
   config = lib.mkIf cfg.enable {
     cattery.brew = {
-      # software that can't update itself.
-      # giving the ablitity to self update is usually more efficient,
-      # tho some software is not able to do so.
       casks = [
         "google-chrome"
         "jetbrains-toolbox"
