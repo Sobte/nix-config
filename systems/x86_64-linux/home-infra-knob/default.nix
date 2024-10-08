d76300e0fe562d81ecf113c7c013638b202f901f
sobte <i@sobte.me>
2024-10-08 20:44:39 +0800

    feat: add ip forward to home-infra-knob

--- systems/x86_64-linux/home-infra-knob/default.nix
+++ systems/x86_64-linux/home-infra-knob/default.nix
@@ -6,7 +6,18 @@
     room.container.enable = true;
     system.proxmox.lxc.enable = true;
     services.wg-quick.configNames = [ "wg-come-home" ];
+    system.boot.kernel.useIpForward = true;
   };
+
+  # ports
+  networking.firewall =
+    let
+      ports = [ 58755 ];
+    in
+    {
+      allowedTCPPorts = ports;
+      allowedUDPPorts = ports;
+    };
 
   system.stateVersion = "24.11";
 }
