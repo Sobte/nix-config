9957cdf2a71f2431d31cff74a3a65be8de2f9ad0
sobte <i@sobte.me>
2024-10-23 23:42:02 +0800

    feat: use forgejo runner

--- systems/x86_64-linux/home-infra-runner-01/services.nix
+++ systems/x86_64-linux/home-infra-runner-01/services.nix
@@ -1,9 +1,11 @@
+{ pkgs, ... }:
 {
   cattery = {
     services = {
       docker.enable = true;
       gitea-actions-runner = {
         enable = true;
+        package = pkgs.forgejo-runner;
         url = "https://git.sobte.dev/";
       };
     };
