6ed8d52915bc53df6b1c9ed17e7d46ee96a24292
sobte <i@sobte.me>
2024-11-20 18:23:02 +0800

    feat: add dolphin user-places to impermanence

--- modules/home/system/impermanence/default.nix
+++ modules/home/system/impermanence/default.nix
@@ -16,10 +16,18 @@ in
 
   config = lib.mkIf cfg.enable {
     cattery.system.impermanence = {
-      xdg.config.directories = [
-        "nix-config"
-        "hosts-secrets"
-      ];
+      xdg = {
+        config.directories = [
+          "nix-config"
+          "hosts-secrets"
+        ];
+        data.files = [
+          # dolphin user-places
+          "user-places.xbel"
+          "user-places.xbel.bak"
+          "user-places.xbel.tbcache"
+        ];
+      };
     };
   };
 }
