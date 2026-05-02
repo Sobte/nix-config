ce58cbc8a44fbf4a5f992c7caa4ddc5f757df73a
sobte <i@sobte.me>
2026-05-03 00:24:07 +0800

    feat: add deployment host

--- colmena/colmenaHive/deployments.nix
+++ colmena/colmenaHive/deployments.nix
@@ -4,4 +4,29 @@
       targetHost = "home-infra-dev";
     };
   };
+  home-infra-dns = {
+    deployment = {
+      targetHost = "home-infra-dns";
+    };
+  };
+  home-infra-knob = {
+    deployment = {
+      targetHost = "home-infra-knob";
+    };
+  };
+  home-infra-pwd = {
+    deployment = {
+      targetHost = "home-infra-pwd";
+    };
+  };
+  home-infra-relay = {
+    deployment = {
+      targetHost = "home-infra-relay";
+    };
+  };
+  home-infra-runner-01 = {
+    deployment = {
+      targetHost = "home-infra-runner-01";
+    };
+  };
 }
