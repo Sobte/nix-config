a860a90ae7790321e3a911d559bd83329851e74b
sobte <i@sobte.me>
2026-05-08 16:17:42 +0800

    feat: flake update

--- overlays/code-cursor/default.nix
+++ overlays/code-cursor/default.nix
@@ -1,9 +1,7 @@
 _: _: prev: {
   code-cursor = prev.code-cursor.overrideAttrs (old: {
-    installPhase =
-      old.installPhase
-      + ''
-        substituteInPlace $out/share/applications/cursor-url-handler.desktop --replace-fail "/usr/share/cursor/cursor" "$out/bin/cursor"
-      '';
+    installPhase = old.installPhase + ''
+      substituteInPlace $out/share/applications/cursor-url-handler.desktop --replace-fail "/usr/share/cursor/cursor" "$out/bin/cursor"
+    '';
   });
 }
