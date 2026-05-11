573a0d19247e862d0ad8fe8071c3f29df4695d96
sobte <i@sobte.me>
2026-05-11 16:09:25 +0800

    feat: use input secrets

--- systems/aarch64-darwin/home-code-mbp/default.nix
+++ systems/aarch64-darwin/home-code-mbp/default.nix
@@ -1,14 +1,11 @@
 {
   cattery = {
+    nix.secrets.enable = true;
     room.desktop.dev.enable = true;
     services = {
       wg-quick = {
         enable = true;
         configNames = [ "wg-go-home" ];
-      };
-      sing-box = {
-        enable = true;
-        # secrets.owner = "meow";
       };
     };
   };
