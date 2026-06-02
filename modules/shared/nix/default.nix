320d29bca46d78feaf31e37ebe408533cb171344
sobte <i@sobte.me>
2026-06-03 00:57:39 +0800

    feat: remove garnix. sad :(

--- modules/shared/nix/default.nix
+++ modules/shared/nix/default.nix
@@ -18,12 +18,10 @@ in
     nix = {
       settings = {
         substituters = [
-          "https://cache.garnix.io"
           "https://hyprland.cachix.org"
         ];
 
         trusted-public-keys = [
-          "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         ];
       };
